f02921c246aa0d2b9692778a2facd0dbde21d5d7
Your Name <you@example.com>
2022-11-10 21:10:23 +0500

    game

--- controls.py
+++
@@ -1,9 +0,0 @@
-import pygame
-import sys
-
-def events():
-    """обработка событий"""
-
-    for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
--- main.py
+++ main.py
@@ -1,20 +1,45 @@
-import pygame, controls
-from roscet import Gun
+import pygame
+from random import randrange
 
-def run():
+res = 800
+size = 50
 
-    pygame.init()
-    screen = pygame.display.set_mode((1200, 800))
-    pygame.display.set_caption("Космические корабли")
-    bg_color = (0, 0, 0)
-    gun = Gun(screen)
+x,y = randrange(0, res, size), randrange(0, res, size)
+apple = randrange(0, res, size), randrange(0, res, size)
+lenght = 1
+snake = [(x,y)]
+dx, dy = 0, 0
+fps = 5
+
+pygame.init()
+
+sc = pygame.display.set_mode([res, res])
+clock = pygame.time.Clock()
+
+while True:
+    sc.fill(pygame.Color('black'))
+    [(pygame.draw.rect(sc, pygame.Color('green'), (i, j, size, size))) for i, j, in snake]
+    pygame.draw.rect(sc, pygame.Color('red'), (*apple, size, size))
+
+    x += dx * size
+    y += dy * size
+    snake.append((x, y))
+    snake = snake[-lenght:]
+
+    pygame.display.flip()
+    clock.tick(fps)
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            exit()
 
 
-    while True:
-        
-        controls.events()
-        screen.fill(bg_color)
-        gun.output()
-        pygame.display.flip()
-
-run()
+    key = pygame.key.get_pressed()
+    if key[pygame.K_w]:
+        dx, dy = 0, -1
+    if key[pygame.K_s]:
+        dx, dy = 0, 1
+    if key[pygame.K_a]:
+        dx, dy = -1, 0
+    if key[pygame.K_d]:
+        dx, dy = 1, 0
